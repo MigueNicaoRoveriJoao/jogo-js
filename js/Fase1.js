34b7a293998cdbf3ceb1d2d7001e7be7b18a81f6
MigueNicaoRoveriJoao <145687792+MigueNicaoRoveriJoao@users.noreply.github.com>
2023-09-21 16:55:49 -0300

    Pulo do personagem com gravidade

--- js/Fase1.js
+++ js/Fase1.js
@@ -61,49 +61,67 @@ const layers = [
 ];
 
 // Variáveis de posição do personagem
-let posicaoHorizontal = 10;
-let posicaoVertical = 10;
+let posicaoHorizontal = 0;
+let posicaoVertical = 452;
+let velocidadeVertical = 0;
+const GRAVIDADE = 1;
 const step = 10;
+const MAX_ALTURA_PULO = 200; // Ajuste para a altura máxima desejada
 
 // Imagem do personagem
 const personagem = new Image();
 personagem.src = '/assets/personagem.png';
 
 // Evento de carregamento do personagem
+
 personagem.onload = function () {
   // Função para desenhar o personagem
   function desenharPersonagem() {
-    ctx.drawImage(personagem, posicaoHorizontal, posicaoVertical, 65, 75); // Define a largura e altura do personagem
+    ctx.drawImage(personagem, posicaoHorizontal, posicaoVertical, 48, 48); // Define a largura e altura do personagem
   }
 
   // Função para atualizar a posição do personagem
   function updatePersonagemPosition() {
+    
+    // Aplicar gravidade para simular a queda
+  velocidadeVertical += GRAVIDADE;
+  posicaoVertical += velocidadeVertical;
+
+  // Limitar a posição vertical para evitar que o personagem "caia" infinitamente
+  if (posicaoVertical >= CANVAS_HEIGHT - 48) {
+    posicaoVertical = CANVAS_HEIGHT - 48;
+    velocidadeVertical = 0; // Reiniciar a velocidade vertical após atingir o chão
+  }
+  if (posicaoVertical <= CANVAS_HEIGHT - MAX_ALTURA_PULO) {
+    posicaoVertical = CANVAS_HEIGHT - MAX_ALTURA_PULO;
+    velocidadeVertical = 0; // Defina a velocidade vertical como zero quando atingir a altura máxima
+  }
     posicaoHorizontal = Math.max(posicaoHorizontal, 0);
     posicaoVertical = Math.max(posicaoVertical, 0);
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     layers.forEach(layer => layer.draw()); // Redesenhe todas as camadas de fundo
     desenharPersonagem();
+
+     // Solicita uma nova animação na próxima atualização do quadro
+  requestAnimationFrame(updatePersonagemPosition);
   }
 
   // Event listener para capturar as teclas pressionadas
   document.addEventListener('keydown', (event) => {
     switch (event.key) {
       case 'ArrowUp':
-        posicaoVertical -= step;
+        if (posicaoVertical === CANVAS_HEIGHT - 48) {
+          velocidadeVertical = -20; // Defina uma velocidade vertical negativa para simular um pulo
+        }
         break;
       case 'ArrowDown':
-        posicaoVertical += step;
-        break;
-      case 'ArrowLeft':
-        posicaoHorizontal -= step;
-        break;
-      case 'ArrowRight':
-        posicaoHorizontal += step;
+        posicaoVertical += step; // Verifique se 'step' é um valor adequado
         break;
     }
-
+  
     updatePersonagemPosition();
   });
+  
 
   // Função para animar o fundo
   function animate() {
